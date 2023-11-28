a3d5222b1f9b401d8562dfa5b0b9743488761b24
M. Bohdan <marynych.b.o@gmail.com>
2023-11-28 17:17:43 +0200

    Added Main slider

---
+++ js/main.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const slides = document.querySelector('.slides');
+	const pagination = document.querySelector('.pagination');
+	let currentIndex = 0;
+
+	// Create dots for pagination
+	for (let i = 0; i < slides.children.length; i++) {
+		const dot = document.createElement('div');
+		dot.classList.add('dot');
+		dot.addEventListener('click', () => goToSlide(i));
+		pagination.appendChild(dot);
+	}
+
+	// Highlight the current dot
+	function updatePagination() {
+		const dots = pagination.children;
+		for (let i = 0; i < dots.length; i++) {
+			dots[i].classList.toggle('active', i === currentIndex);
+		}
+	}
+
+	// Go to the specified slide index
+	function goToSlide(index) {
+		currentIndex = index;
+		const translateValue = -index * 100 + '%';
+		slides.style.transform = 'translateY(' + translateValue + ')';
+		updatePagination();
+	}
+
+	// Automatic slide change every 3 seconds
+	setInterval(() => {
+		currentIndex = (currentIndex + 1) % slides.children.length;
+		goToSlide(currentIndex);
+	}, 3000);
+
+	// Initial setup
+	updatePagination();
+});
